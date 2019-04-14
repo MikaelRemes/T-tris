3c1de8f7d2d86e22660d083c9d0ead890e85bccd
pojankolli <pojankolli@users.noreply.github.com>
2019-04-14 17:32:29 +0300

    Started branch

---
+++ mainpackage/MainMenu.java
@@ -0,0 +1,16 @@
+package mainpackage;
+import javax.swing.JFrame;
+
+
+public class MainMenu extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3L;
+
+	public MainMenu() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- mainpackage/TetrisStarter.java
+++ mainpackage/TetrisStarter.java
@@ -1,11 +1,12 @@
 package mainpackage;
 
 public class TetrisStarter {
+	
 	public static void main(String[] args) {
 		System.out.println("Hello Tetris");
 		//Start up the UI and the game
 		@SuppressWarnings("unused")
 		UserInterface gameUI = new UserInterface();
 	}
-
 }
+
